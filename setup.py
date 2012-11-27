3faf2c2530f8105dbcc1cebb95ef635eb228816a
Ryan Kelly <ryan@rfk.id.au>
2012-11-27 17:04:36 +1100

    Bump development version number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid >= 1.3', 'macauthlib >= 0.3.0', 'tokenlib', 'webtest']
 
 setup(name='pyramid_macauth',
-      version='0.3.0',
+      version='0.4.0.dev1',
       description='pyramid_macauth',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
